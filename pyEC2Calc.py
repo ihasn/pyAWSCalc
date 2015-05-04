a97ea5010d9c85de1891da9db66bd58c04010f34
ppiersonbt <ppierson@bericotechnologies.com>
2015-05-03 22:12:14 -0400

    added testing kinda

--- pyEC2Calc.py
+++ pyEC2Calc.py
@@ -1,16 +1,22 @@
-__author__ = 'izen'
+__author__ = 'ihasn'
+# Simple EC2 calculator for AWS.
+
 # Imports
 import csv
 
-# Main Function
-def main():
+
+def osinput():
     # Takes input as either Linux or Windows
     os = raw_input("Linux/Windows OS: ")
     if os != 'Linux' and os != 'Windows':
         # Exits if not
         print 'Exiting...'
         exit()
+    print ""
+    return os
 
+
+def printinstances(os):
     # Sets osfile name for loading of prices
     osfile = 'Pricing' + os + '.csv'
 
@@ -19,14 +25,16 @@ def main():
         reader = csv.DictReader(csvfile)
         for row in reader:
             print row['Name'] + ",",
-
-    # Cause need space
     print ""
+    return osfile
+
+
+def assigninstancetype():
     print ""
+    return raw_input("Instance Type: ")
 
-    # Sets Instance type
-    instance = raw_input("Instance Type: ")
 
+def calccost(os, osfile, instance):
     # Opens osfile and reads through rows
     with open(osfile) as csvfile:
         reader = csv.DictReader(csvfile)
@@ -44,13 +52,46 @@ def main():
                 print "Using Reserved pricing a " + os + " " + instancetype + " costs $" + str(rhfloat) + " an hour." + " $" + \
                     str(rhfloat * 24) + " a day." + " $" + str(rhfloat * 24 * 30) + " a month." + \
                     " $" + str(rhfloat * 24 * 30 * 12) + " a year."
-                print "With a savings of $" + str(odhfloat - rhfloat) + " an hour at " + str(round(rhfloat / odhfloat, 2) * 100) + "%."
-
-    # Cause need space
+                print "With a savings of $" + str(odhfloat - rhfloat) + " an hour at " + \
+                    str(round(rhfloat / odhfloat, 2) * 100) + "%."
     print ""
+
+
+# Test Function
+def test():
+    print 'Testing Linux OS type'
+    os = 'Linux'
+    osfile = printinstances(os)
+    print 'Testing m3.medium instance type'
+    instance = 'm3.medium'
+    calccost(os, osfile, instance)
+    print 'Testing Windows OS type'
+    os = 'Windows'
+    osfile = printinstances(os)
+    print 'Testing m2.xlarge instance type'
+    instance = 'm2.xlarge'
+    calccost(os, osfile, instance)
+    # TODO: Fix this faulty input
+    # print 'Testing faulty OS type'
+    # os = 'Test'
+
+
+# Main Function
+def main():
+
+    os = osinput()
+    osfile = printinstances(os)
+
+    # Sets Instance type
+    instance = assigninstancetype()
+
+    calccost(os, osfile, instance)
+
     # Rerun main?
     main()
 
+
 # Call main
 if __name__ == '__main__':
-    main()
+    test()
+    # main()
